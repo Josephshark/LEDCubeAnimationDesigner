be03ec7e74405992892f24d98e998c3e344a51fb
Joseph Bagheri <jbagheri@uwaterloo.ca>
2023-12-29 21:48:49 -0500

    create a event loop todo.

--- python/ledvisualised.py
+++ python/ledvisualised.py
@@ -1,9 +1,15 @@
 #https://matplotlib.org/stable/plot_types/3D/scatter3d_simple.html
-
 import ledcube as lc
+import time
 
 n = 5
 cube = lc.LedCube(n)
 cube.change_led([0,0,0], 5)
 cube.change_led([3,2,2], 5)
+cube.show()
+
+time.sleep(1)
+
+cube.change_led([0,0,1], 5)
+cube.change_led([3,0,2], 5)
 cube.show()
