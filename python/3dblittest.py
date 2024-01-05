bde3d2c9ec24b3e528d862cd61832eb86fd916ff
Joseph Bagheri <jbagheri@uwaterloo.ca>
2024-01-05 11:50:00 -0500

    Attempt 3d blitting animation.

---
+++ python/3dblittest.py
@@ -0,0 +1,9 @@
+from blitmanager import BlitManager
+import matplotlib.pyplot as plt
+import numpy as np
+
+# make a new figure
+plt.style.use('_mpl-gallery')
+
+# Plot
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
