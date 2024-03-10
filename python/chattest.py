f3749d1d08570bb2399a185382fb11c4442a9448
Joseph Bagheri <jbagheri@uwaterloo.ca>
2024-03-10 17:51:18 -0400

    This is working animation other than certain colors off.

--- python/chattest.py
+++ python/chattest.py
@@ -4,27 +4,38 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 import ledcube as lc
 
+n = 5
+cube = lc.LedCube(n)
+cube1 = cube
 
+cube.change_led([0,0,0], 5) # Replace this with your own data
+cube2 = cube
 
-data = np.random.rand(3, 10)  # Replace this with your own data
+# create a columns around the first led
+for z in range(n):
+    cube.change_led([1,1,z], 5)
+    cube.change_led([0,1,z], 5)
+    cube.change_led([1,0,z], 5)
+
+cube3 = cube
+
+cubes = [cube1, cube2]
 
 # Function to initialize the plot
 def init():
-    ax.scatter(data[0], data[1], data[2])
+    scatter = ax.scatter(cube1.x, cube1.y, cube1.z, c = cube1.brightness)
     return (ax,)
 
 # Function to update the plot at each frame
 def update(frame):
     # Update the data for each frame (replace this with your own data update logic)
-    data[0] += 0.1
-    data[1] += 0.2
-    data[2] += 0.3
+    # update the index
     
     # Clear the previous scatter plot
     ax.cla()
     
     # Plot the updated data
-    ax.scatter(data[0], data[1], data[2])
+    ax.scatter(cubes[frame].x, cubes[frame].y, cubes[frame].z, c = cubes[frame].brightness)
     
     # Set labels for the axes
     ax.set_xlabel('X-axis')
@@ -38,7 +49,7 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # Set the initial data
-scatter = ax.scatter(data[0], data[1], data[2])
+scatter = ax.scatter(cube1.x, cube1.y, cube1.z, c = cube1.brightness)
 
 # Set labels for the axes
 ax.set_xlabel('X-axis')
@@ -46,7 +57,7 @@ ax.set_ylabel('Y-axis')
 ax.set_zlabel('Z-axis')
 
 # Create the animation
-animation = FuncAnimation(fig, update, frames=range(50), init_func=init, blit=False, interval=100)
+animation = FuncAnimation(fig, update, frames=range(2), init_func=init, blit=False, interval=1000)
 
 # Show the animation
 plt.show()
